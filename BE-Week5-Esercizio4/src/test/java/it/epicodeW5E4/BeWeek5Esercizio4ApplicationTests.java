891409314bb02ccdc045007d3fe264e641019a1b
Alessia Lacitignola <alelaci21@gmail.com>
2023-02-09 18:10:43 +0100

    BE-Week5-Esercizio4

---
+++ BE-Week5-Esercizio4/src/test/java/it/epicodeW5E4/BeWeek5Esercizio4ApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicodeW5E4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeWeek5Esercizio4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
